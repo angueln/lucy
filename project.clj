91d6c4c356b993819566933bd05f3eb511b214b4
Anguel Nikolov <nikolov.anguel@gmail.com>
2016-06-25 07:18:18 +0300

    Add dependencies.

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject lucy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Parallelized fractal generation."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.commons/commons-math3 "3.6.1"]
+                 [org.apfloat/apfloat "1.8.2"]
+                 [commons-cli/commons-cli "1.3.1"]]
   :main ^:skip-aot lucy.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
