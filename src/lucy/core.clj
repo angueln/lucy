7da42b4e1c9372d6d54d5f07e16cca6ed0facc87
Anguel Nikolov <nikolov.anguel@gmail.com>
2016-06-25 21:27:17 +0300

    Single threaded generation.

--- src/lucy/core.clj
+++ src/lucy/core.clj
@@ -1,28 +1,83 @@
 (ns lucy.core
   (:gen-class))
 
-(import 'org.apache.commons.math3.complex.Complex)
+(import 'org.apache.commons.math3.complex.Complex
+        'java.io.File
+        'java.awt.Color
+        'java.awt.image.BufferedImage
+        'javax.imageio.ImageIO)
 
-(defn iterator-map
+(defn exp-cos-x-c
   "The next number in the sequence a_n = exp(cos(c a_{n-1})), for some
   constant c."
   [c a]
   (.exp (.cos (.multiply a c))))
 
-(defn bounded-sequence?
+(defn iterations-to-bounded
   "Checks if the orbit map-fun(map-fun( ... map-fun(0))) of 0 under
   iteration of map-fun *looks* bounded - i.e. does it reach Infinity
   after iteration-limit iterations."
   [map-fun iteration-limit]
-  (let [orbit-prefix (take iteration-limit
-                           (iterate map-fun (Complex. 0.0 0.0)))
-        finite? #(not (or (.isInfinite %)
+  (let [finite? #(not (or (.isInfinite %)
                           (.isNaN      %)))]
-    (every? finite?
-            (map #(.abs %)
-                 orbit-prefix))))
+    (loop [point (Complex. 0.0 0.0)
+           index 0]
+      (if (= index iteration-limit)
+        nil
+        (if (finite? point)
+          (recur (map-fun point) (inc index))
+          index)))))
+
+(defn draw-fractal
+  "Returns cavas-width by canvas-height BufferedImage of fractal set
+  described by iterator-map in real-range and imaginary-range.
+  Boundedness determined for iteration-limit iterations."
+  [iterator-map
+   canvas-width
+   canvas-height
+   real-range
+   imaginary-range
+   iteration-limit]
+  (let [image          (BufferedImage. canvas-width canvas-height
+                                       BufferedImage/TYPE_3BYTE_BGR)
+        pixel-to-point (fn [pixel canvas-size [lower-bound upper-bound]]
+                         (+ lower-bound (* (/ pixel canvas-size)
+                                           (- upper-bound lower-bound))))]
+    (doseq [x (range canvas-width)
+            y (range canvas-height)
+            :let [c (Complex. (pixel-to-point x canvas-width  real-range)
+                              (pixel-to-point y canvas-height imaginary-range))
+                  iterator (partial iterator-map c)]]
+      (println "Calculating pixel [ " x ", " y " ]")
+
+      (if-let [iterations (iterations-to-bounded iterator iteration-limit)]
+        (do
+          (println "Found to be unbounded after " iterations " iterations.")
+          (.setRGB image x y (+ 0xFFFF00 (* 255
+                              (- 1 (/ iterations
+                                      iteration-limit))))))
+        (do
+          (println "Bounded.")
+          (.setRGB image x y 0x0000FF))))
+    image))
+
+(def output-file "fractal")
+(def canvas-width 3000)
+(def canvas-height 3000)
+(def real-range [-2.0 2.0])
+(def imaginary-range [-2.0 2.0])
+(def iteration-limit 10)
+(def num-threads 4)
+(def output-file "fractal")
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (ImageIO/write (draw-fractal exp-cos-x-c
+                               canvas-width
+                               canvas-height
+                               real-range
+                               imaginary-range
+                               iteration-limit)
+                 "png"
+                 (File. (str output-file ".png"))))
